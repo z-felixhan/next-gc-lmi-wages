4a8e5d567497cc0e5ab4585ceb34f55f2f1870cf
Felix <34609664+z-felixhan@users.noreply.github.com>
2021-11-29 02:29:51 -0500

    Get & display wages

--- components/Form.js
+++ components/Form.js
@@ -1,43 +1,71 @@
 import { Autocomplete, Button, Grid, TextField } from "@mui/material";
 
 const Form = (props) => {
-  const { noc, nocList, setNocSelected } = props;
+  const { noc, nocList, nocSelected, pruidList, setNocSelected, setWages } =
+    props;
+
+  const getWage = async (e, noc) => {
+    e.preventDefault();
+    setWages([]);
+
+    const url = `https://orient.onrender.com/gc/wages/${noc}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    const add = await data.map((item) => {
+      const name = pruidList.filter((x) => String(x.pruid) == item.pruid)[0]
+        .name;
+      return { ...item, name };
+    });
+    const result = await add.sort((a, b) => a.name.localeCompare(b.name));
+
+    console.log(result);
+    setWages(result);
+  };
 
   return (
-    <>
-      <Grid container>
-        <Autocomplete
-          id="noc"
-          getOptionLabel={(option) => `${option.noc} - ${option.title}`}
-          inputValue={noc}
-          onChange={(e, value) => {
-            if (value) {
-              setNocSelected(value.noc);
-            } else {
-              setNocSelected("");
-            }
-          }}
-          options={nocList}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              InputLabelProps={{ shrink: true }}
-              label="NOC - Job Title"
-              fullWidth
-              required
-            />
-          )}
-          sx={{ width: 920 }}
-          value={noc}
-          autoHighlight
-          autoComplete
-          disablePortal
-        />
-        <Button type="submit" variant="contained">
-          Get Wage
-        </Button>
+    <form onSubmit={(e) => getWage(e, nocSelected)}>
+      <Grid
+        container
+        spacing={1}
+        direction="row"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Grid item xs={12} sm={8} md={8} lg={8}>
+          <Autocomplete
+            id="noc"
+            getOptionLabel={(option) => `${option.noc} - ${option.title}`}
+            inputValue={noc}
+            onChange={(e, value) => {
+              if (value) {
+                setNocSelected(value.noc);
+              } else {
+                setNocSelected("");
+              }
+            }}
+            options={nocList}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                InputLabelProps={{ shrink: true }}
+                label="NOC - Job Title"
+                required
+              />
+            )}
+            value={noc}
+            autoHighlight
+            autoComplete
+            disablePortal
+            fullWidth
+          />
+        </Grid>
+        <Grid item>
+          <Button type="submit" sx={{ padding: "1rem" }} variant="contained">
+            Get Wage
+          </Button>
+        </Grid>
       </Grid>
-    </>
+    </form>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,11 @@ import Form from "../components/Form";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
+var formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "CAD",
+});
+
 export async function getStaticProps() {
   const nocUrl = "https://orient.onrender.com/noc";
   const pruidUrl = "https://orient.onrender.com/pruid";
@@ -14,14 +19,22 @@ export async function getStaticProps() {
 
   // Get PRUID
   const pruidResponse = await fetch(pruidUrl);
-  const pruid = await pruidResponse.json();
-
-  return { props: { nocList, pruid } };
+  const pruidUnsorted = await pruidResponse.json();
+  const pruidList = await pruidUnsorted
+    .slice()
+    .sort((a, b) => a.name.localeCompare(b.name));
+  console.log(pruidList);
+  return {
+    props: {
+      nocList,
+      pruidList,
+    },
+  };
 }
 
-export default function Home({ nocList, pruid }) {
+export default function Home({ nocList, pruidList }) {
   const [nocSelected, setNocSelected] = useState("");
-  const [title, setTitle] = useState("");
+  const [wages, setWages] = useState([]);
 
   return (
     <div className={styles.container}>
@@ -40,11 +53,39 @@ export default function Home({ nocList, pruid }) {
           <Form
             nocList={nocList}
             nocSelected={nocSelected}
+            pruidList={pruidList}
             setNocSelected={setNocSelected}
+            setWages={setWages}
+            wages={wages}
           />
         </div>
 
-        <div className={styles.grid}></div>
+        <div className={styles.grid}>
+          {wages.length
+            ? wages.map((wage) => {
+                const {
+                  eruid,
+                  max_wage,
+                  med_wage,
+                  min_wage,
+                  name,
+                  noc,
+                  pruid,
+                  ref_period,
+                } = wage;
+
+                return (
+                  <div key={eruid} className={styles.card}>
+                    <h2>{name}</h2>
+                    <p>{formatter.format(max_wage)}</p>
+                    <p>{formatter.format(med_wage)}</p>
+                    <p>{formatter.format(min_wage)}</p>
+                    <p>Updated: {ref_period}</p>
+                  </div>
+                );
+              })
+            : ""}
+        </div>
       </main>
 
       <footer className={styles.footer}>© Felix</footer>
