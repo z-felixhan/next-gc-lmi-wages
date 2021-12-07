25b81938010926e8beaf27df6c34406848924b93
Felix <34609664+z-felixhan@users.noreply.github.com>
2021-12-06 19:50:35 -0500

    Add favicon for GitHub Pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link
+          rel="shortcut icon"
+          href="https://z-felixhan.github.io/next-gc-lmi-wages/favicon.ico"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
