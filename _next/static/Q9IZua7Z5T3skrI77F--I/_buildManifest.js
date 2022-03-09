54d8c1ed6c3fb52e85eeb7248bd0ee5369fa345c
z-felixhan <34609664+z-felixhan@users.noreply.github.com>
2022-03-09 01:39:29 +0000

    Deploying to gh-pages from @ z-felixhan/next-gc-lmi-wages@59f7e1b66b7501e1aeba823a16eb149c2c4fdc8a 🚀

---
+++ _next/static/Q9IZua7Z5T3skrI77F--I/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/334-c66a5788f5926587.js","static/css/6d919dafa79792e3.css","static/chunks/pages/index-8ff2bdc10def669b.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
